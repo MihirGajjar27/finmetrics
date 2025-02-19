3d392639af0917725c0646a522a4481e79ee4617
MihirGajjar27 <165854969+MihirGajjar27@users.noreply.github.com>
2025-02-19 00:18:52 -0800

    New Functionality Added: - holding_period_return function created - holding_period_return_multi_period created

--- finmetrics/main.py
+++ finmetrics/main.py
@@ -1,2 +1,31 @@
 
-def holding_period_return(beginning_value, ending_value, )
+# Basic Return Calculations
+def holding_period_return(beginning_value: float, ending_value: float, total_income: float = 0.0) -> float:
+    """
+    :param beginning_value: starting value of an underlying asset
+    :type beginning_value: float
+
+    :param ending_value: ending value of an underlying asset
+    :type ending_value: float
+
+    :param total_income:
+    :type total_income: float
+
+    :return: holding period return for a period between: time @ ending value & time @ beginning value
+    :rtype: float
+    """
+    return (ending_value - beginning_value + total_income) / beginning_value
+
+
+def holding_period_return_multi_period(annual_returns: "list[float] | np.ndarray") -> float:
+    """
+    :param annual_returns: A list or numpy ndarray of annual returns for a series of periods
+    :type annual_returns: list[float] || numpy ndarray
+
+    :return: Holding Period Return for a wide range of periods
+    :rtype: float
+    """
+    accumulator = 1
+    for i in annual_returns:
+        accumulator *= (1 + i)
+    return accumulator - 1
